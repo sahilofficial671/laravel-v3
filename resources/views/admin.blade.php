229f0e4f63d913ed3dc24460b4d0d0ddc59a0eb8
Sahil Bhatia <developer@wowfactors.net>
2020-08-04 14:50:42 +0530

    Commit Again

---
+++ resources/views/admin.blade.php
@@ -0,0 +1,34 @@
+@extends('layouts.app-admin')
+
+@section('content')
+<div class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">Admin {{ __('Dashboard') }}</div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                        <div class="alert alert-success" role="alert">
+                            {{ session('status') }}
+                        </div>
+                    @endif
+
+                    {{ __('You are logged in!') }} as <strong>Admins</strong>
+
+                    @if (Auth::guard('web')->check())
+                        <p class="text-success">You are logged in as <strong>User</strong></p>
+                    @else
+                    <p class="text-danger">You are logged out as <strong>User</strong></p>
+                    @endif
+                    @if (Auth::guard('admin')->check())
+                    <p class="text-success">You are logged in as <strong>Admin</strong></p>
+                    @else
+                    <p class="text-danger">You are logged out as <strong>Admin</strong></p>
+                    @endif
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
